2ed3fec39c5cfc954d8c1587f42b1705d05e91a1
nagrohan726 <nag.rohan726@gmail.com>
2021-11-26 00:19:37 +0530

    Utilizing native, local and npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,23 +1,51 @@
-const validator = require('validator')
+// const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
 
-console.log("console.log(validator.isEmail('example.com'))")
-console.log(validator.isEmail('example.com'))
+console.log(chalk.green(msg))
+console.log(chalk.bgGreen('Success!'))
+console.log(chalk.inverse('Inverse'))
+console.log(chalk.bold('Bold'))
 
-console.log("console.log(validator.isEmail('rohan@example.com'))")
-console.log(validator.isEmail('rohan@example.com'))
+console.log(chalk.green.bgGray.bold.italic('Combination'))
 
-console.log("console.log(validator.isURL('nodejs.org'))")
-console.log(validator.isURL('nodejs.org'))
+const green = chalk.green
+console.log(green('Green'))
 
-console.log("console.log(validator.isURL('https/mead.io'))")
-console.log(validator.isURL('https/mead.io'))
+const error = chalk.bold.red
+const warning = chalk.keyword('yellow')
 
-console.log("console.log(validator.isURL('https://mead.io'))")
-console.log(validator.isURL('https://mead.io'))
+console.log(error('Error!!!'))
+console.log(warning('Warning!'))
+
+
+const name = 'Rohan'
+const color = chalk.inverse.keyword('blue')
+
+console.log(color('%s... %s'), name, getNotes())
+
+
+
+
+
+
+// console.log("console.log(validator.isEmail('example.com'))")
+// console.log(validator.isEmail('example.com'))
+
+// console.log("console.log(validator.isEmail('rohan@example.com'))")
+// console.log(validator.isEmail('rohan@example.com'))
+
+// console.log("console.log(validator.isURL('nodejs.org'))")
+// console.log(validator.isURL('nodejs.org'))
+
+// console.log("console.log(validator.isURL('https/mead.io'))")
+// console.log(validator.isURL('https/mead.io'))
+
+// console.log("console.log(validator.isURL('https://mead.io'))")
+// console.log(validator.isURL('https://mead.io'))
 
 
 
